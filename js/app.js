14a3430e78be3b1348b0653653b288daa1a1d7b2
ScottyKnows44 <Scottcfalcon@gmail.com>
2018-02-15 20:15:24 -0800

    Added comments to make it readable

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// Global variables
+
 var timeHours = ['6am','7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var storeForm = document.getElementById('storeForm');
 var allBranches = [];
 var tableContent = document.getElementById('storeTable');
 
+// Event listener form
+
 storeForm.addEventListener('submit', handleFormSubmit);
+
+// constructor function
 
 function SalmonStore (name, minCustomers, maxCustomers, avgCookies) {
   this.timeHours = timeHours;
@@ -21,6 +27,17 @@ function SalmonStore (name, minCustomers, maxCustomers, avgCookies) {
   this.totalSold();
 }
 
+// Random generator for people showing up every hour
+
+SalmonStore.prototype.peopleEveryHour = function() {
+  for (var i = 0; i < timeHours.length; i++) {
+    var customerForOneHour =  Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)+ this.minCustomers);
+    this.customersEachHour.push(customerForOneHour);
+  }
+},
+
+// function for total sold for whole day
+
 SalmonStore.prototype.totalSold = function() {
   for (var i=0; i < timeHours.length; i++) {
     var totalCookiesSold = this.customersEachHour[i] * this.avgCookies;
@@ -29,12 +46,8 @@ SalmonStore.prototype.totalSold = function() {
   }
 },
 
-SalmonStore.prototype.peopleEveryHour = function() {
-  for (var i = 0; i < timeHours.length; i++) {
-    var customerForOneHour =  Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)+ this.minCustomers);
-    this.customersEachHour.push(customerForOneHour);
-  }
-},
+// generator for numbers going in table, minus the header & footer row
+
 SalmonStore.prototype.putTheNumbersInTheBroswer = function() {
   var trEL = document.createElement('tr');
   tdEL = document.createElement('td');
@@ -51,6 +64,8 @@ SalmonStore.prototype.putTheNumbersInTheBroswer = function() {
   trEL.appendChild(tdEL);
 };
 
+// event form for a new branch
+
 function handleFormSubmit(event){
   event.preventDefault();
   var avgCookies = parseFloat(event.target.avgCookies.value);
@@ -62,6 +77,8 @@ function handleFormSubmit(event){
   renderTable();
 }
 
+// footer row for all totals, including total of totals
+
 function footerRow() {
   var trEl = document.createElement('tr');
   var total = document.createElement('td');
@@ -72,7 +89,7 @@ function footerRow() {
     var totalPerHour = document.createElement('td');
     var startingCookies = 0;
     for (var j = 0; j < allBranches.length; j++) {
-      startingCookies = allBranches[j].totalCookiesSoldToday[i] + startingCookies;
+      startingCookies += allBranches[j].totalCookiesSoldToday[i];
       totalOfTotals += allBranches[j].totalCookiesSoldToday[i];
     }
     totalPerHour.textContent = startingCookies;
@@ -84,6 +101,8 @@ function footerRow() {
   tableContent.appendChild(trEl);
 }
 
+// header row, includes time of day when open, blank cell, and header for total
+
 function headerRow () {
   var total = document.createElement('th');
   var blankData = document.createElement('th');
@@ -94,16 +113,20 @@ function headerRow () {
     thEL.textContent = timeHours[i];
     trEL.appendChild(thEL);
   }
-  total.textContent = 'total';
+  total.textContent = 'Total';
   trEL.appendChild(total);
   tableContent.appendChild(trEL);
 }
+
+// Core 5 branches that are always there when refreashed
 
 new SalmonStore('1st and Pike', 23, 65, 6.3);
 new SalmonStore('SeaTac Airport', 3, 24, 1.2);
 new SalmonStore('SeattleCenter', 11, 38, 3.7);
 new SalmonStore('Capital Hill', 20, 38, 2.3);
 new SalmonStore('Alki', 2, 16, 4.6);
+
+// deletes whole table then remakes table
 
 function renderTable() {
   tableContent.innerHTML= '';
@@ -114,6 +137,3 @@ function renderTable() {
   footerRow();
 }
 renderTable();
-
-
-
